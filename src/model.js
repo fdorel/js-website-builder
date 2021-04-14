8f6c6dd319148a2bed27b646ec360828d36daddb
fdorel <dorelfurdui@gmail.com>
2021-04-14 17:04:27 -0400

    first commit

---
+++ src/model.js
@@ -0,0 +1,12 @@
+import image from './assets/img01.jpg';
+
+export const model = [
+  {type: 'title', value: 'paragraph'},
+  {type: 'text', value: 'some text'},
+  {type: 'columns', value: [
+    '11111111',
+    '22222222',
+    '33333333'
+  ]},
+  {type: 'image', value: 'image'}
+];
---
+++ src/utils.js
@@ -0,0 +1,7 @@
+export function row() {
+  return `<div class="row">${content}</div>`
+}
+
+export function col() {
+  return `<div class="col-sm">${content}</div>`
+}
